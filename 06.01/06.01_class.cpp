411e1aa3c7af3e879810bea85488d57cff490a5e
lord-of-nothing <77851847+lord-of-nothing@users.noreply.github.com>
2024-11-18 20:24:29 +0000

    add 06.01

---
+++ 06.01/06.01_class.cpp
@@ -0,0 +1,49 @@
+#include "06.01_class.hpp"
+
+Entity::Entity() : m_data_1(s_data_1), m_data_2(0), m_data_3(0) {}
+
+Entity::Entity(const int& data_1, int data_2, int data_3)
+    : m_data_1(data_1), m_data_2(data_2), m_data_3(data_3) {}
+
+Entity::Entity(const int& data_1, int data_2) : Entity(data_1, data_2, 0) {}
+
+Entity::~Entity() { std::clog << "Entity::~Entity\n"; }
+
+
+int Entity::data_1() const { return m_data_1; }
+
+int Entity::data_2() const { return m_data_2; }
+
+int Entity::data_3() const { return m_data_3; }
+
+
+const std::string& Entity::data_as_string() const {
+  //	m_data_3 = 0; // error
+
+  if (m_cached_data.is_invalid) {
+    m_cached_data.update(*this);
+  }
+
+  return m_cached_data.string;
+}
+
+void Entity::set_data_3(int data) {
+  m_data_3 = data;
+
+  m_cached_data.is_invalid = true;
+}
+
+void Entity::test() {
+  std::clog << "Entity::test : s_data_1 = " << s_data_1 << '\n';
+
+  //		std::clog << "Entity::test : m_data_3 = " << m_data_3 << '\n';
+  //// error
+}
+
+void Entity::Cached_Data::update(const Entity& entity) {
+  string = (std::to_string(entity.m_data_1) + '/' +
+            std::to_string(entity.m_data_2) + '/' +
+            std::to_string(entity.m_data_3));
+
+  is_invalid = false;
+}
---
+++ 06.01/06.01_class.hpp
@@ -0,0 +1,43 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+using namespace std::literals;
+
+class Entity {
+ public:
+  Entity();
+  Entity(const int& data_1, int data_2, int data_3);
+  Entity(const int& data_1, int data_2);
+  ~Entity();
+
+  int data_1() const;
+  int data_2() const;
+  int data_3() const;
+
+  const std::string& data_as_string() const;
+
+  void set_data_3(int data);
+
+  static void test();
+
+ private:
+  struct Cached_Data {
+    void update(const Entity& entity);
+
+    std::string string;
+    bool is_invalid = true;
+  };
+
+
+  static inline int s_data_1 = 0;
+  static const int s_data_2 = 0;
+  static inline const std::string s_data_4 = "aaaaa"s;
+
+
+  const int& m_data_1 = 0;
+  const int m_data_2 = 0;
+  int m_data_3 = 0;
+
+  mutable Cached_Data m_cached_data;
+};
