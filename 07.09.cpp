ab2ac2867186772dfc70c173c484b66861385267
lord-of-nothing <77851847+lord-of-nothing@users.noreply.github.com>
2024-12-11 15:37:22 +0000

    update 07.09.cpp

--- 07.09.cpp
+++ 07.09.cpp
@@ -1,35 +1,61 @@
 #include <benchmark/benchmark.h>
 
-class Testing {
+class NonVirtual {
  public:
-
-  void bar() {
+  __attribute__((noinline))void bar(int& start) {
     int b = 0;
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = start; i < start + 100; ++i) {
       benchmark::DoNotOptimize(b += i);
     }
+    ++start;
   }
-  virtual void foo() {
-    int a = 0;
-    for (int i = 0; i < 1000; ++i) {
-      benchmark::DoNotOptimize(a += i);
-    }
-  }
-
-  virtual ~Testing() = default;
 };
 
-Testing t;
+class Virtual {
+ public:
+  __attribute__((noinline))virtual void foo(int& start) {
+    int a = 0;
+    benchmark::DoNotOptimize(a);
+    for (int i = start; i < start + 100; ++i) {
+      benchmark::DoNotOptimize(a += i);
+    }
+    ++start;
+  }
+
+  virtual ~Virtual() = default;
+};
+
+class ChildVirtual : public Virtual {
+ public:
+  void foo(int& start) override {
+    int c = 0;
+    for (int i = start; i < start + 100; ++i) {
+      benchmark::DoNotOptimize(c += i);
+    }
+    junk = 1;
+    ++start;
+  }
+ private:
+  int junk = 0;
+};
 
 static void BM_NonVirtual(benchmark::State& state) {
+  NonVirtual a;
+  int start = 1;
   for (auto _ : state) {
-    t.bar();
+    a.bar(start);
   }
 }
 
 static void BM_Virtual(benchmark::State& state) {
+  int start = 0;
+  ChildVirtual cv;
+  cv.foo(start);
+  benchmark::DoNotOptimize(cv);
+
+  Virtual a;
   for (auto _ : state) {
-    t.foo();
+    a.foo(start);
   }
 }
 
@@ -37,8 +63,3 @@ BENCHMARK(BM_NonVirtual);
 BENCHMARK(BM_Virtual);
 
 BENCHMARK_MAIN();
-
-/* хотелось бы сделать выводы, но значения при каждом запуске
- * получаются разными, причём быстрее оказывается то один, то другой вариант
- * и разница почти всегда мала
- * разобраться, с чем это может быть связано, пока не получилось */
